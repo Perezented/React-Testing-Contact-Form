c5bafd633f8a42669b8b0b45e1a6bf192463f094
Michael Perez <mperez6473@gmail.com>
2020-05-07 18:09:34 -0500

    Added an act for the fireEvents as I keep gettin an error telling me to fix it.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,31 +1,41 @@
 import React from 'react';
 
 import ContactForm from './ContactForm';
-import { render, fireEvent, getByLabelText } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { act } from 'react-dom/test-utils';
 
 test('ContactForm renders', () => {
     render(<ContactForm />);
 });
 
-test('able to fill out form and submit', () => {
-    const { getByTestId, getByText, getByLabelText } = render(<ContactForm />);
+test('able to fill out form and submit', async () => {
+    const { getByTestId, getByText, getByLabelText, getAllByText } = render(
+        <ContactForm />
+    );
 
-    fireEvent.change(getByTestId(/firstname/i), {
-        target: { name: 'firstName', value: 'first name here' },
-    });
+    act(() => {
+        fireEvent.change(getByTestId(/firstname/i), {
+            target: { name: 'firstName', value: 'first name here' },
+        });
 
-    fireEvent.change(getByTestId(/lastname/i), {
-        target: { name: 'lastName', value: 'last name to go here' },
+        fireEvent.change(getByTestId(/lastname/i), {
+            target: { name: 'lastName', value: 'last name to go here' },
+        });
+        fireEvent.change(getByTestId(/email/i), {
+            target: { name: 'email', value: 'email@email.com' },
+        });
+        fireEvent.change(getByTestId(/message/i), {
+            target: {
+                name: 'message',
+                value: 'gimme fake message here please',
+            },
+        });
+        fireEvent.click(getByTestId(/submit/i));
+
+        expect;
     });
-    fireEvent.change(getByTestId(/email/i), {
-        target: { name: 'email', value: 'email@email.com' },
-    });
-    fireEvent.change(getByTestId(/message/i), {
-        target: { name: 'message', value: 'gimme fake message here please' },
-    });
-    console.log(getByText(/email/i).value);
-    console.log(getByLabelText(/email/i).value);
-    console.log(getByTestId(/email/i).value);
-    fireEvent.click(getByTestId(/submit/i));
+    // console.log(getByText(/email/i).value);
+    // console.log(getByLabelText(/email/i).value);
+    // console.log(getByTestId(/email/i).value);
 });
