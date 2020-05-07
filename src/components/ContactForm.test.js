4236508dd69cc60d22621d9f61d754e5aa3f4993
Michael Perez <mperez6473@gmail.com>
2020-05-07 16:19:26 -0500

    Getting an error as there is no await at the moment

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import ContactForm from './ContactForm';
 import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 test('ContactForm renders', () => {
     render(<ContactForm />);
 });
 
 test('Submit returns an error if inputs are left blank', () => {
-    const { getByTestId } = render(<ContactForm />);
+    const { getByTestId, getAllByText, toBeInTheDocument } = render(
+        <ContactForm />
+    );
     fireEvent.click(getByTestId(/submit/i));
+    expect(getAllByText('error').toBeInTheDocument());
 });
